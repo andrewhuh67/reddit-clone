1ed3637219915075ea8c0a40d0a3539a8a779939
Jason Pruski <jprus@localhost.localdomain>
2018-05-08 23:22:17 -0400

    basic stuff from app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+// set view engine
+app.set('view engine', 'ejs');
+
+
+const path = require("path");
+app.use(express.static(
+  path.join(__dirname, 'public')
+));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+    console.log('app now listening for requests on port 3000');
+});
